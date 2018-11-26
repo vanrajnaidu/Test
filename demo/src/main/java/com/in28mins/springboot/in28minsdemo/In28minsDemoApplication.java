bf92e52f019ff09b6c0a586f12c2b031dd9bfb9d
vanraj naidu <vanraj.naidu@gmail.com>
2018-11-26 14:43:20 +0530

    COMMIT ALL

---
+++ demo/src/main/java/com/in28mins/springboot/in28minsdemo/In28minsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.in28mins.springboot.in28minsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minsDemoApplication.class, args);
+	}
+}
---
+++ demo/src/test/java/com/in28mins/springboot/in28minsdemo/In28minsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28mins.springboot.in28minsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class In28minsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
